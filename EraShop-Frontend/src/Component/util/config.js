029d1ada2741b3e3a3d523e7ded5aee3e2460f00
ismael <ismaeleliper@gmail.com>
2024-05-10 16:54:00 -0300

    test

--- EraShop-Frontend/src/Component/util/config.js
+++ EraShop-Frontend/src/Component/util/config.js
@@ -1,4 +1,4 @@
-export const baseURL = "https://painel.livepay.app.br/";
+export const baseURL = "http://localhost:5000";
 export const key = "D-lRnkhY#D_w`@DGnsRHik85}sy{Ex}n0<of|5[{o.-CZG_Skl35l[gh:$Pi+|l";
 export const ProjectName = "LivePay";
 

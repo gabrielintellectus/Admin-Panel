0ec170b92e511e86d5f25eed48dc01f0f4aed568
gabriel <intellectusgabriel@gmail.com>
2024-06-18 18:52:29 -0300

    email template

--- EraShop-Backend/server/payment/email.service.js
+++ EraShop-Backend/server/payment/email.service.js
@@ -30,38 +30,42 @@ exports.send = async (email, userName, orderId) => {
       tab += `<div style="margin:50px auto;width:70%;padding:20px 0"><div style="border-bottom:1px solid #eee">`;
       tab += `<a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Hi, ${userName}</a>`;
       tab += `</div>`;
-      tab += `<div style="padding:20px;">`;
-    
+      tab += `<div>`;
+      
       tab += `<h3>Order Details</h3>`;
-      tab += `<p>Order ID: ${order.orderId}</p>`;
-      tab += `<p>Total Quantity: ${order.totalQuantity}</p>`;
-      tab += `<p>Total Items: ${order.totalItems}</p>`;
-      tab += `<p>Total Shipping Charges: ${order.totalShippingCharges}</p>`;
-      tab += `<p>SubTotal: ${order.subTotal}</p>`;
-      tab += `<p>Total: ${order.total}</p>`;
-      tab += `<p>Final Total: ${order.finalTotal}</p>`;
-    
+      tab += `<table style="width:100%;border-collapse:collapse;">`;
+      tab += `<tr><td>Order ID:</td><td>${order.orderId}</td></tr>`;
+      tab += `<tr><td>Total Quantity:</td><td>${order.totalQuantity}</td></tr>`;
+      tab += `<tr><td>Total Items:</td><td>${order.totalItems}</td></tr>`;
+      tab += `<tr><td>Total Shipping Charges:</td><td>R$ ${order.totalShippingCharges}</td></tr>`;
+      tab += `<tr><td>SubTotal:</td><td>R$ ${order.subTotal}</td></tr>`;
+      tab += `<tr><td>Total:</td><td>R$ ${order.total}</td></tr>`;
+      tab += `<tr><td>Final Total:</td><td>R$ ${order.finalTotal}</td></tr>`;
+      tab += `</table>`;
+      tab += `<hr style="border:none;border-top:1px solid #eee" />`
+      
       tab += `<h3>Items:</h3>`;
+      tab += `<table style="width:100%;border-collapse:collapse;">`;
       order.items.forEach(item => {
-        tab += `<p>Product ID: ${item.productId}</p>`;
-        tab += `<p>Product Code: ${item.productCode}</p>`;
-        tab += `<p>Product Quantity: ${item.productQuantity}</p>`;
-        tab += `<p>Purchased Time Product Price: ${item.purchasedTimeProductPrice}</p>`;
-        tab += `<p>Purchased Time Shipping Charges: ${item.purchasedTimeShippingCharges}</p>`;
-        tab += `<h5>Status: ${item.status}</h5>`;
-        tab += `<hr style="border:none;border-top:1px solid #eee" />`;
+        tab += `<tr><td>Product ID:</td><td>${item.productId}</td></tr>`;
+        tab += `<tr><td>Product Code:</td><td>${item.productCode}</td></tr>`;
+        tab += `<tr><td>Product Quantity:</td><td>${item.productQuantity}</td></tr>`;
+        tab += `<tr><td>Purchased Time Product Price:</td><td>R$ ${item.purchasedTimeProductPrice}</td></tr>`;
+        tab += `<tr><td>Purchased Time Shipping Charges:</td><td>${item.purchasedTimeShippingCharges}</td></tr>`;
+        tab += `<tr><td>Status:</td><td style="${item.status === "Confirmed"? "color: green" : "color: red"}">${item.status}</td></tr>`;
+        tab += `<tr><td colspan="2"><hr style="border:none;border-top:1px solid #eee" /></td></tr>`;
       });
-    
+      tab += `</table>`;
+      
       tab += `</div>`;
       tab += `<div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">`;
       tab += `</div></div></div></body></html>`;
-    
-  
+      
       //mail details
       var mailOptions = {
         from: `${config.EMAIL}`,
         to: email,
-        subject: "Sending Email for Password Security",
+        subject: `LivePay Order Details`,
         html: tab,
       };
   
